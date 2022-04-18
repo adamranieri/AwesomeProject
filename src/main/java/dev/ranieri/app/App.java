1cb2e600d1c1a5360b2ed3bca422d19b7c78966a
AdamRanieri <adaman94@gmail.com>
2022-04-18 10:17:35 -0400

    Made some basic changes!

--- src/main/java/dev/ranieri/app/App.java
+++ src/main/java/dev/ranieri/app/App.java
@@ -3,5 +3,6 @@ package dev.ranieri.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("wassup");
+        System.out.println("Revised!!!!");
     }
 }
---
+++ src/main/java/dev/ranieri/app/Something.java
@@ -0,0 +1,4 @@
+package dev.ranieri.app;
+
+public class Something {
+}
